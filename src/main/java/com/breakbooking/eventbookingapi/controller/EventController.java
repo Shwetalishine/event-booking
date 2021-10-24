e4d746a28c8d179937af7ac460af2c78c01006c0
Shwetali <shwetalishine@gmail.com>
2021-10-24 15:59:37 +1100

    new feature added

--- src/main/java/com/breakbooking/eventbookingapi/controller/EventController.java
+++ src/main/java/com/breakbooking/eventbookingapi/controller/EventController.java
@@ -32,12 +32,6 @@ public class EventController {
 		this.eventService = eventService;
 	}
 
-	/* JENKINS TESTING */
-
-	@GetMapping("/helloWorld")
-	public void get(){
-		System.out.println("Hello World!");
-	}
 
 
 	/* FIND ALL EVENTS */
